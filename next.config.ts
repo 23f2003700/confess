02c91611ae5c394a1c99bdabe52df63ea0883366
23f2003700 <23f2003700@ds.study.iitm.ac.in>
2025-12-19 16:26:28 +0530

    Add catbox.moe to allowed image domains

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,16 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'files.catbox.moe',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.catbox.moe',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: '*.s3.ap-south-1.amazonaws.com',
         pathname: '/**',
       },
